f0233fb273a460bdd8d9b64b03caf672c83355ef
rajbsahaj <rajb@sahajsoft.com>
2020-02-22 11:41:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,180 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = [
+        {
+            'name': 'Stage1',
+        },
+        {
+            'name': 'Stage2',
+        }
+    ];
+    var treeData =
+    {
+        "name": "Top Level",
+        "children": [
+            {
+                "name": "Level 2: A",
+                "children": [
+                    { "name": "Son of A" },
+                    { "name": "Daughter of A" }
+                ]
+            },
+            { "name": "Level 2: B" }
+        ]
+    };
+    const boxes = data.map(stage => (<div>{stage.name}</div>));
+
+
+    return (
+        <div className="App">
+            <div id="main">
+                <div id="left">
+                    <div id="top">
+                        <ul class="round round-1">
+                            <li class="knocked-out">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="selected">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                        <ul class="round round-2">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                        </ul>
+                        <ul class="round round-3">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                    </div>
+
+                    <div id="bottom">
+                        <ul class="round round-1">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                        <ul class="round round-2">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                        <ul class="round round-3">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="center">
+                    WINNER
+                </div>
+                <div id="right">
+                    <div id="top">
+                        <ul class="round round-3">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                        <ul class="round round-2">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+
+                        <ul class="round round-1">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                    </div>
+                    <div id="bottom">
+                        <ul class="round round-3">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                        <ul class="round round-2">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+
+                        <ul class="round round-1">
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                            <li class="after-space">&nbsp;</li>
+                            <li class="">Team name</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
